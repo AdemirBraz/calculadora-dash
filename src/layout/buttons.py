25a116ff93e856e6129529e55be599899acb3a8e
Bruno Oshiro <75125546+BrunoOshiro27@users.noreply.github.com>
2026-05-20 20:01:42 +0900

    dica

--- src/layout/buttons.py
+++ src/layout/buttons.py
@@ -1,10 +1,45 @@
 import dash_bootstrap_components  as dbc
 from dash import html
 
+lista = []
+simbolos = ["/", "x", ...]
+
+def btnss():
+        for i in range(0, 10):
+                lista.append(dbc.Button(i, id={"type": "btn", "index": i}))
+
+        for pos, simbolo in enumerate(simbolos):
+                dbc.Button(simbolo, id={"type": "operador", "index": pos})
+        return lista # [ botao1, botao2 ...]
+
 def create_buttons():
         return dbc.Col([
-                dbc.Col(dbc.ButtonGroup([dbc.Button('7',id='btn-7'),dbc.Button('8'),dbc.Button('9'),dbc.Button('/')],id='botoes',class_name='w-100 p-1')),
-                dbc.Col(dbc.ButtonGroup([dbc.Button('4'),dbc.Button('5'),dbc.Button('6'),dbc.Button('*')],id='botoes',class_name='w-100 p-1')),
-                dbc.Col(dbc.ButtonGroup([dbc.Button('1'),dbc.Button('2'),dbc.Button('3'),dbc.Button('-')],id='botoes',class_name='w-100 p-1')),
-                dbc.Col(dbc.ButtonGroup([dbc.Button('0'),dbc.Button('.'),dbc.Button('='),dbc.Button('+')],id='botoes',class_name='w-100 p-1')),
+                dbc.Col(
+                        dbc.ButtonGroup([
+                                dbc.Button('7', id={"type": "btn", "index": 0}),
+                                dbc.Button('8', id={"type": "btn", "index": 1}),
+                                dbc.Button('9'),
+                                dbc.Button('/')
+                        ], id='botoes-1',class_name='w-100 p-1')),
+                dbc.Col(
+                        dbc.ButtonGroup([
+                                dbc.Button('4'),
+                                dbc.Button('5'),
+                                dbc.Button('6'),
+                                dbc.Button('*')
+                        ],class_name='w-100 p-1')),
+                dbc.Col(
+                        dbc.ButtonGroup([
+                                dbc.Button('1'),
+                                dbc.Button('2'),
+                                dbc.Button('3'),
+                                dbc.Button('-')
+                        ],class_name='w-100 p-1')),
+                dbc.Col(
+                        dbc.ButtonGroup([
+                                dbc.Button('0'),
+                                dbc.Button('.'),
+                                dbc.Button('='),
+                                dbc.Button('+')
+                        ],class_name='w-100 p-1')),
         ], className='g-2')      
--- src/main.py
+++ src/main.py
@@ -1,24 +1,28 @@
 from layout.buttons import create_buttons
-from dash import Dash, html, dcc, callback, Output, Input,State,dash
+from dash import Dash, html, dcc, callback, Output, Input,State, ALL
 from layout.case import estrutura
 import dash_bootstrap_components  as dbc
 
-app = Dash(__name__,
-           external_stylesheets=[dbc.themes.BOOTSTRAP])
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 botoes = create_buttons
 
 app.layout = html.Div([
     html.Div(children=estrutura(), id='my-div'),
     dcc.Button(id='botoes',n_clicks=0),
-    html.Div(id=f"btn-{botoes}"),
+    html.Div(id="btn-1", ),
             
     ])
-app.callback(
-   Output("my-div", "children"),
-   Input('btn-7','n_clicks'),
+
+
+
+@app.callback(
+   Output("btn", "children"),
+   Input({"type": "btn", "index": ALL}, "n_clicks"),
 )
 def update_display(value):
+    if not value:
+        return
     return value
 
 if __name__ == "__main__":
