3a633bc5ee34e2dff948fbfb78b2593680dac679
AdemirBraz <gordoademir2@gmail.com>
2026-05-21 01:11:51 +0900

    consegui fazer os numeros aparecer de acordo com os numeros da calculadora

--- src/layout/buttons.py
+++ src/layout/buttons.py
@@ -5,31 +5,31 @@ def create_buttons():
     return dbc.Col([
         dbc.Col(
             dbc.ButtonGroup([
-                dbc.Button('C', id={"type": "clear", "index": 0}, color="danger"),
+                dbc.Button('C', id="clear", color="danger"),
                 dbc.Button('7', id={"type": "btn", "index": 7}),
                 dbc.Button('8', id={"type": "btn", "index": 8}),
                 dbc.Button('9', id={"type": "btn", "index": 9}),
-                dbc.Button('/', id={"type": "operador", "index": 1})
+                dbc.Button('/', id={"type": "operador", "index": '/'})
             ], id='botoes-1', class_name='w-100 p-1')),
         dbc.Col(
             dbc.ButtonGroup([
                 dbc.Button('4', id={"type": "btn", "index": 4}),
                 dbc.Button('5', id={"type": "btn", "index": 5}),
                 dbc.Button('6', id={"type": "btn", "index": 6}),
-                dbc.Button('*', id={"type": "operador", "index": 2})
+                dbc.Button('*', id={"type": "operador", "index": '*'})
             ], class_name='w-100 p-1')),
         dbc.Col(
             dbc.ButtonGroup([
                 dbc.Button('1', id={"type": "btn", "index": 1}),
                 dbc.Button('2', id={"type": "btn", "index": 2}),
                 dbc.Button('3', id={"type": "btn", "index": 3}),
-                dbc.Button('-', id={"type": "operador", "index": 3})
+                dbc.Button('-', id={"type": "operador", "index":'-'})
             ], class_name='w-100 p-1')),
         dbc.Col(
             dbc.ButtonGroup([
                 dbc.Button('0', id={"type": "btn", "index": 0}),
-                dbc.Button('.', id={"type": "operador", "index": 4}),
-                dbc.Button('=', id={"type": "operador", "index": 5}),
-                dbc.Button('+', id={"type": "operador", "index": 6})
+                dbc.Button('.', id={"type": "operador", "index": '.'}),
+                dbc.Button('=', id={"type": "operador", "index": '='}),
+                dbc.Button('+', id={"type": "operador", "index": '+'})
             ], class_name='w-100 p-1'))
     ], className='g-2')
--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from layout.buttons import create_buttons
-from dash import Dash, html, dcc, callback, Output, Input, State, ALL
+from dash import Dash, html, dcc, callback, Output, Input, State, ALL,ctx
 from layout.case import estrutura
 import dash_bootstrap_components as dbc
 
@@ -10,21 +10,22 @@ botoes = create_buttons
 app.layout = html.Div([
     html.Div(children=estrutura(), id='my-div'),
     dcc.Button(id='btn-1', n_clicks=0),
-    html.Div(id="btn",),
+    html.Div(id="btn",)
 ])
 
 @app.callback(
     Output("text", "value"),
     Input({"type": "btn", "index": ALL}, "n_clicks"),
     Input({"type": "operador", "index": ALL}, "n_clicks"),
-    Input({"type": "clear", "index": ALL}, "n_clicks"),
+    Input("clear", "n_clicks"),
+    State('text','value'),
+    prevent_initial_call=True
 )
-def update_display(btn_clicks, operador_clicks, clear_clicks):
-    if not btn_clicks and not operador_clicks:
-        return btn_clicks and operador_clicks
-    if clear_clicks:
-        return ""
 
-    return clear_clicks
+def update_display(btn_clicks, operador_clicks, clear_clicks,valor_input):    
+   trigger=ctx.triggered_id
+   print(trigger['index'])
+   valor_input+=1
+   return trigger['index']
 if __name__ == "__main__":
     app.run(debug=True)
