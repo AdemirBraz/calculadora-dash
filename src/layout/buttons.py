33e4de38522f1f06426604460719cd8f690d6f9e
Bruno Oshiro <75125546+BrunoOshiro27@users.noreply.github.com>
2026-05-26 01:13:59 +0900

    arrumei o layout em partes

--- src/layout/buttons.py
+++ src/layout/buttons.py
@@ -6,23 +6,25 @@ from logic.constant import operador
 #     for num in range(0, 10):
 #         numeros.append(dbc.Button(num, id= {"type":"btn", "index":num}, color = "dark"))
 
+ultima_linha = [0, ".", "="]
+
 def create_buttons():
-    numeros = [dbc.Button(num, id= {"type":"btn", "index":num}, color = "dark") for num in range(9, 0, -1)]
+    numeros = [dbc.Button(num, id= {"type":"btn", "index":num}, color = "dark", class_name="w-25 m-1") for num in range(9, 0, -1)]
     operadores = [dbc.Button(opr,id= {"type":"operador","index":opr})for opr in operador]
+    last_row = [dbc.Button(symbol, id= {"type":"btn", "index":symbol}, color = "dark", class_name="w-25 m-1") for symbol in ultima_linha]
     return dbc.Row([
         dbc.Col([
             dbc.Row(
-                numeros,class_name='w-50 p-1 justify-content-left'
+                numeros
             ),
             dbc.Row(
-                dbc.Button(0, id= {"type":"btn", "index":"0"}, color = "dark")
+                last_row
             )
-        ]),
+        ], width=9),
         dbc.Col(
             operadores
         )
-    ])
-    return 
+    ], class_name="px-1 mt-3")
 
     return dbc.Col([
         dbc.Col(
