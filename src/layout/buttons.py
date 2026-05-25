3348f59a308f395f201ed4daa7a87002708f8c65
AdemirBraz <gordoademir2@gmail.com>
2026-05-26 00:54:16 +0900

    fix buttons

--- src/layout/buttons.py
+++ src/layout/buttons.py
@@ -1,14 +1,28 @@
 import dash_bootstrap_components as dbc
 from dash import html
-
+from logic.constant import operador
 # def create_buttons():
 #     numeros = []
 #     for num in range(0, 10):
 #         numeros.append(dbc.Button(num, id= {"type":"btn", "index":num}, color = "dark"))
 
 def create_buttons():
-    numeros = [dbc.Button(num, id= {"type":"btn", "index":num}, color = "dark") for num in range(0, 10)]
-    return dbc.ButtonGroup(numeros)
+    numeros = [dbc.Button(num, id= {"type":"btn", "index":num}, color = "dark") for num in range(9, 0, -1)]
+    operadores = [dbc.Button(opr,id= {"type":"operador","index":opr})for opr in operador]
+    return dbc.Row([
+        dbc.Col([
+            dbc.Row(
+                numeros,class_name='w-50 p-1 justify-content-left'
+            ),
+            dbc.Row(
+                dbc.Button(0, id= {"type":"btn", "index":"0"}, color = "dark")
+            )
+        ]),
+        dbc.Col(
+            operadores
+        )
+    ])
+    return 
 
     return dbc.Col([
         dbc.Col(
